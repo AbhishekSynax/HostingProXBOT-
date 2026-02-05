4ae43cca8c3452473b52af508fa10069ce3a9dc5
AbhishekSynax <synaxbots@gmail.com>
2026-02-06 02:04:35 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -44,7 +44,7 @@ def keep_alive():
 # --- End Flask Keep Alive ---
 
 # --- Configuration ---
-TOKEN = '8217597110:AAFzXFAUHYvp5Qv4wXkeQY2Dal64suFc5vE'
+TOKEN = '8217597110:AAHBC3H2nAuan-7hjTcc6SwM2Cn5AQxnB9M'
 OWNER_ID = 6068463116
 ADMIN_ID = 6068463116
 YOUR_USERNAME = '@verifiedmanu'
