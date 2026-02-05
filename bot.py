00f54b397c83519f2e7d23511b2afbe799681494
AbhishekSynax <synaxbots@gmail.com>
2026-02-06 02:18:44 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -44,7 +44,7 @@ def keep_alive():
 # --- End Flask Keep Alive ---
 
 # --- Configuration ---
-TOKEN = '8217597110:AAHBC3H2nAuan-7hjTcc6SwM2Cn5AQxnB9M'
+TOKEN = '8572313080:AAHqgHkgY-GpKuyZTsjo1b9ShbsOi6-dBR8'
 OWNER_ID = 6068463116
 ADMIN_ID = 6068463116
 YOUR_USERNAME = '@synaxchatrobot'
